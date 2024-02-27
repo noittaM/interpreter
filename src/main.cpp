fa276224d10aea73101514a8ee0879d628dafd1e
Mattion <omarturkmah@gmail.com>
2024-02-27 15:38:26 +0200

    removed unnecessary comments.

--- src/main.cpp
+++ src/main.cpp
@@ -7,52 +7,6 @@
 #include "./Tokenizer.hpp"
 #include "./Parser.hpp"
 
-
-
-
-
-
-// std::string tokenToStr (const std::vector<Token> tokens) {
-//     std::string output{};
-//     for (int i{0}; i < tokens.size(); ++i) {
-//         const Token& token = tokens[i]; 
-//         switch (token.type)
-//         {
-//         case TokenType::_return:
-//             if (i + 1 < tokens.size() && (tokens[i+1].type == TokenType::int_lit || tokens[i+1].type == TokenType::expression))
-//                 if (i + 2 < tokens.size () && tokens[i+2].type == TokenType::semi) {
-//                     output += "return " + tokens[i+1].value + ";\n"; 
-//                     i += 2;
-//                 }
-//                 else {
-//                     std::cerr << "Expected ';'.\n";
-//                     exit(EXIT_FAILURE);
-//                 }
-//             else {
-//             std::cerr << "Expected expression.\n";
-//             exit(EXIT_FAILURE);
-//             }
-//             break;
-
-//         case TokenType::int_lit:
-//             std::cerr << "What do I do with int literal: " << token.value << "????.\n";
-//             exit(EXIT_FAILURE);
-
-//         case TokenType::semi:
-//             std::cerr << "What do I do with semi: ';'????.\n";
-//             exit(EXIT_FAILURE);
-        
-//         default:
-//             std::cerr << "No idea what this token is.\n";
-//             std::cerr << "Token value: '" << token.value << "'.\n";
-//             std::cerr << "Token type: '" << token.type << "'.\n";
-//             exit(EXIT_FAILURE);
-//             break;
-//         }
-//     }
-//     return output;
-// }
-
 void writeAsCpp (const std::string& str) {
     std::string output;
     output = "int main() {\n";
