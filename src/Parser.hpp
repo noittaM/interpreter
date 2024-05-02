7b8104f4cfc26778e0f044a6f5c05f68cb591a7e
Mattion <omarturkmah@gmail.com>
2024-05-02 21:58:44 +0300

    everything is broken fix it lol

--- src/Parser.hpp
+++ src/Parser.hpp
@@ -10,9 +10,11 @@
 
 #include "Tokenizer.hpp"
 
+using int_literal = int;
+using identifier = std::string;
 
 struct NodeTerm {
-    std::variant < int, const std::string* > term;
+    std::variant <int_literal, const identifier> term;
 };
 
 struct BinaryOperator {
@@ -53,16 +55,16 @@ struct NodeBinaryExpr {
 struct NodeStatement;
 
 struct NodeScope {
-    std::vector < NodeStatement* > statements;
+    std::vector <NodeStatement *> statements;
 };
 
 struct NodeExpr {
-    std::variant < NodeTerm*, NodeBinaryExpr* > expression;
+    std::variant <NodeTerm, NodeBinaryExpr *> expression;
 };
 
 struct NodeDefineVar{
     const std::string& identifier;
-    std::optional < NodeExpr* > value; // TODO: Types
+    std::optional <NodeExpr *> value; // TODO: Types
 };
 
 struct NodeAssignVar {
@@ -84,7 +86,7 @@ struct NodeStatement {
 };
 
 struct NodeProgram {
-    std::vector < NodeStatement* > statements;
+    std::vector <NodeStatement *> statements;
 };
 
 class Parser {
@@ -95,18 +97,17 @@ public:
     }
 
     // TODO: copy constructor and assignment
-    ~Parser() {
-        for (const auto& stmt : m_prog.statements) {
-            delete stmt;
-        }
-        // m_prog.statements.clear();
-    }
+    // ~Parser() {
+    //     for (const auto& stmt : m_prog.statements) {
+    //         delete stmt;
+    //     }
+    //     // m_prog.statements.clear();
+    // }
 
     NodeProgram parse() {
-        while (peekToken().has_value()) {
-            NodeStatement* stmt = new  NodeStatement(getNodeStatement());
-            std::cout << stmt->statement.index() << std::endl;
-            m_prog.statements.push_back(stmt);
+        while (std::optional<NodeStatement *> stmt { getNodeStatement() }) {
+            // std::cout << stmt->statement.index() << std::endl;
+            m_prog.statements.push_back(stmt.value());
         }
         return m_prog; 
     }
@@ -116,16 +117,375 @@ private:
     size_t m_index{};
     NodeProgram m_prog;
 
+private:
     std::optional<Token> peekToken(size_t offset = 0) {
         if (m_index + offset < m_inTokens.size())
             return m_inTokens[m_index + offset];
         return {};
     }
 
-    void take() {
+    void consumeToken() {
         m_index++;
     }
 
+    std::optional<NodeStatement *> getNodeStatement() {
+        NodeStatement* stmt = nullptr;
+
+        if (std::optional<NodeDefineVar *> node = getNodeDefineVar()) {
+            stmt->statement = node.value();
+        } else if (std::optional<NodeAssignVar *> node = getNodeAssignVar()) {
+            stmt->statement = node.value();
+        } else if (std::optional<NodeReturn *> node = getNodeReturn()) {
+            stmt->statement = node.value();
+        } else {
+            return {};
+        }
+
+        return stmt;
+    }
+
+    std::optional<NodeDefineVar *> getNodeDefineVar() {
+        if (!peekToken().has_value() 
+            || peekToken().value().type != TokenType::let) {
+            return {};
+        }
+
+        consumeToken();
+
+        // expect identifier after 'let'
+        if (!peekToken().has_value()
+            || peekToken().value().type != TokenType::identifier) {
+            std::cerr << "Expected identifier after let\n";
+            exit(EXIT_FAILURE);
+        }
+        const std::string ident { peekToken().value().value.value() };
+        consumeToken();
+
+        // valid variable declaration with no value
+        if (peekToken().has_value()
+            && peekToken().value().type == TokenType::semi) {
+            consumeToken();
+            NodeDefineVar* node { new NodeDefineVar {
+                .identifier { ident }, // NOTE: copying strings around
+                .value { {} }
+            }};
+            return node;
+        }
+
+        // expect '=' after indentifier
+        if (!peekToken().has_value()
+            || peekToken().value().type != TokenType::equal) {
+            std::cerr << "Expected '=' in identifier definition\n";
+            exit(EXIT_FAILURE);
+        }
+        consumeToken();
+
+        // expect expression after '='
+        std::optional<NodeExpr *> expr { getNodeExpr() };
+        if (!expr.has_value()) {
+            std::cerr << "Expected expression\n";
+            exit(EXIT_FAILURE);
+        }
+
+        // expect ';'
+        if (!peekToken().has_value()
+            || peekToken().value().type != TokenType::semi) {
+            std::cerr << "Expected ';'\n";
+            exit(EXIT_FAILURE);
+        }
+        consumeToken();
+
+        // NOTE: copying strings but whatever
+        NodeDefineVar* node { new NodeDefineVar { ident, expr.value() } };
+        return node;
+    }
+
+    std::optional<NodeAssignVar *> getNodeAssignVar() {
+        if (!peekToken().has_value()) {
+            return {};
+        }
+
+        if (peekToken().value().type != TokenType::identifier
+            || peekToken(1).has_value()
+            || peekToken(1).value().type == TokenType::equal
+        ) {
+            return {};
+        }
+        const std::string ident { peekToken().value().value.value() };
+        consumeToken(); // token identifier
+        consumeToken(); // token equal
+
+        // expect expression after '='
+        std::optional<NodeExpr *> expr { getNodeExpr() };
+        if (!expr.has_value()) {
+            std::cerr << "Expected expression\n";
+            exit(EXIT_FAILURE);
+        }
+
+        // expect ';'
+        if (!peekToken().has_value()
+            || peekToken().value().type != TokenType::semi) {
+            std::cerr << "Expected ';'\n";
+            exit(EXIT_FAILURE);
+        }
+        consumeToken();
+
+        // NOTE: copying strings but whatever
+        NodeAssignVar* node { new NodeAssignVar { ident, expr.value() } };
+        return node;
+    }
+
+    std::optional<NodeReturn *> getNodeReturn() {
+        if (!peekToken().has_value()
+            || peekToken().value().type != TokenType::_return) {
+            return {};
+        }
+        consumeToken();
+
+        // expect expression
+        std::optional<NodeExpr *> expr { new NodeExpr { getNodeExpr() } };
+        if (!expr.has_value()) {
+            std::cerr << "Expected expression after 'return'\n";
+            exit(EXIT_FAILURE);
+        }
+
+        // expect ';'
+        if (!peekToken().has_value()
+            || peekToken().value().type != TokenType::semi) {
+            std::cerr << "Expected ';' after return expression\n";
+            exit(EXIT_FAILURE);
+        }
+        consumeToken();
+
+        NodeReturn* node { new NodeReturn { expr.value() } };
+        return node;
+    }
+
+    std::optional<NodeExpr *> getNodeExpr() {
+        // term
+        // binary expression
+        NodeExpr* expr;
+        // define var
+        // assign var
+        // return
+        if (std::optional<NodeTerm> node = getNodeTerm()) {
+            expr->expression = node.value();
+        } else if (std::optional<NodeBinaryExpr *> node = getNodeBinaryExpr()) {
+            expr->expression = node.value();
+        } else {
+            return {};
+        }
+    }
+
+    std::optional<NodeTerm> getNodeTerm() {
+        if (!peekToken().has_value()) {
+            return {};
+        }
+
+        NodeTerm term;
+
+        if (peekToken().value().type == TokenType::identifier
+            && !isOperator(peekToken(1))) {
+            // NOTE: copying strings around again
+            term.term.emplace<const identifier>(peekToken().value().value.value());
+            return term;
+        } else if (peekToken().value().type == TokenType::int_lit) {
+            int valueInt { std::stoi(peekToken().value().value.value()) };
+            term.term.emplace<int_literal>(valueInt);
+            return term;
+        } else {
+            return {};
+        }
+    }
+
+    bool isOperator(std::optional<Token> token) {
+        if (!token.has_value()) {
+            return false;
+        }
+
+        if (token.value().type == TokenType::plus
+            || token.value().type == TokenType::dash
+            || token.value().type == TokenType::asterisk
+            || token.value().type == TokenType::forward_slash
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    bool isTerm(std::optional<Token> token) {
+        if (!token.has_value()) {
+            return false;
+        }
+
+        if (token.value().type == TokenType::identifier
+            || token.value().type == TokenType::int_lit
+        ) {
+            return true;
+        }
+        return false;
+    }
+    std::optional<NodeBinaryExpr *> getNodeBinaryExpr() {
+        /*
+        example:
+        5 + 5 - 3 + 9
+        vvvvv
+        binaryAdd(5, 5) - 3 + 9
+                    vvvvvv
+        binarySub(binaryAdd(5, 5), 3) + 9
+                                vvvvvvvvv
+        binaryAdd(binarySub(binaryAdd(5, 5), 3), 9)
+        */
+
+
+        //*check if there is a term
+        // if yes then make sure there is an operator
+        // if either is not found, return {}
+        // if both found then expect a right term
+        // if not found explode
+        // if found make a biexper depending on the operator
+        // repeat *
+        NodeExpr* leftOperand;
+        NodeExpr* rightOperand;
+        NodeBinaryExpr* binaryExpr;
+
+        bool foundExpr = false;
+
+        while (!isTerm(peekToken()) || !isOperator(peekToken(1))) {
+
+
+            // expect a right operand since we found a left operand and operator
+            if (!isTerm(peekToken(2))) {
+                std::cerr << "Expected right operand\n";
+            }
+
+            leftOperand->expression.emplace<NodeTerm>(NodeTerm {
+                .term { peekToken().value().value.value() }
+            });
+
+            TokenType _operator { peekToken(1).value().type };
+
+            rightOperand->expression.emplace<NodeTerm>(NodeTerm {
+                .term { peekToken(2).value().value.value() }
+            });
+
+            consumeToken(); // left operand
+            consumeToken(); // operator
+            consumeToken(); // right operand
+
+
+            if (_operator == TokenType::plus) {
+                NodeBinaryExprAdd* node {
+                    new NodeBinaryExprAdd { leftOperand, rightOperand }
+                };
+                binaryExpr->binaryExpr.emplace<NodeBinaryExprAdd *>(node);
+                foundExpr = true;
+
+            } else if (_operator == TokenType::dash) {
+                NodeBinaryExprSub* node {
+                    new NodeBinaryExprSub { leftOperand, rightOperand }
+                };
+                binaryExpr->binaryExpr.emplace<NodeBinaryExprSub *>(node);
+                foundExpr = true;
+
+            } else if (_operator == TokenType::asterisk) {
+                NodeBinaryExprMult* node {
+                    new NodeBinaryExprMult { leftOperand, rightOperand }
+                };
+                binaryExpr->binaryExpr.emplace<NodeBinaryExprMult *>(node);
+                foundExpr = true;
+
+            } else if (_operator == TokenType::forward_slash) {
+                NodeBinaryExprDiv* node {
+                    new NodeBinaryExprDiv { leftOperand, rightOperand }
+                };
+                binaryExpr->binaryExpr.emplace<NodeBinaryExprDiv *>(node);
+                foundExpr = true;
+
+            } else {
+                std::cerr << "Invalid operator.\n";
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        if (foundExpr) {
+            return binaryExpr;
+        }
+        return {};
+
+
+
+
+
+        #if 0
+        std::optional<NodeTerm> leftTerm { getNodeTerm().value() };
+        if (!leftTerm.has_value()) {
+            return {};
+        }
+
+        // not calling take() yet as there is still a chance of returning nothing
+        if (!peekToken().has_value()) {
+            return {};
+        }
+
+        NodeBinaryExpr binaryExpr;
+        NodeExpr leftOperand { &leftTerm.value() };
+        NodeTerm rightTerm {0};
+        NodeExpr rightOperand { &rightTerm };
+        NodeBinaryExprAdd node { &leftOperand, &rightOperand };
+        binaryExpr.binaryExpr.emplace<NodeBinaryExprAdd*>(&node);
+
+        bool foundExpression = false;
+        while (true) {
+            std::optional<TokenType> _operator { peekToken(1).value().type };
+            if (!_operator.has_value() || !isOperator(_operator.value())) {
+                break;
+            }
+            std::optional<NodeTerm> rightTerm { getNodeTerm(2) };
+            if (!rightTerm.has_value()) {
+                break;
+            }
+            take();
+            take();
+            // don't take a third time
+            NodeExpr leftOperand { &binaryExpr }; // look at example
+            NodeExpr rightOperand { &rightTerm.value() }; //get next right operand
+            if (_operator == TokenType::plus) {
+                NodeBinaryExprAdd node { &leftOperand, &rightOperand };
+                binaryExpr.binaryExpr.emplace<NodeBinaryExprAdd*>(&node);
+                foundExpression = true;
+            } else if (_operator == TokenType::dash) {
+                NodeBinaryExprSub node { &leftOperand, &rightOperand };
+                binaryExpr.binaryExpr.emplace<NodeBinaryExprSub*>(&node);
+                foundExpression = true;
+            } else if (_operator == TokenType::asterisk) {
+                NodeBinaryExprMult node { &leftOperand, &rightOperand };
+                binaryExpr.binaryExpr.emplace<NodeBinaryExprMult*>(&node);
+                foundExpression = true;
+            } else if (_operator == TokenType::forward_slash) {
+                NodeBinaryExprDiv node { &leftOperand, &rightOperand };
+                binaryExpr.binaryExpr.emplace<NodeBinaryExprDiv*>(&node);
+                foundExpression = true;
+            } else {
+                std::cerr << "Invalid operator.\n";
+                exit(EXIT_FAILURE);
+            }
+        }
+        if (foundExpression) {
+            take(); // take() to because we wored with an extra token
+            return binaryExpr;
+        }
+        // if there was no expression (out biExrp = binaryAdd(5, 0))
+        // return nothing and don't take()
+        return {};
+        #endif
+    }
+
+
+
+
+
+    #if 0
     std::optional<NodeTerm> tryGetNode() {
         NodeTerm node;
         if (!peekToken().has_value()) {
@@ -545,6 +905,7 @@ private:
     }
     #endif
 
+    #endif
 
 };
 
